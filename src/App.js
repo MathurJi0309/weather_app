8d0a3ac20bf4fead596a175abe81d85cbde6b935
MathurJi <anileshjjssacademy84@gmail.com>
2023-04-03 11:26:49 +0530

    add the api and its working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import Buttons from "./Components/Buttons";
+import Forecast from "./Components/Forecast";
 import Input from "./Components/Input";
 import Location from "./Components/Location";
 import Temp from "./Components/Temp";
+import getWeatherData from "./api/weatherapi";
 
 function App() {
+
+  const fetchData=async ()=>{
+    const data=await getWeatherData({q:'varanasi'});
+    console.log(data);
+  }
+
+  fetchData();
+
+
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <Buttons/>
       <Input/>
       <Location/>
       <Temp/>
+      <Forecast/>
 
     </div>
   );
--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -40,9 +40,28 @@ const Temp = () => {
             Wind:
             <span className="font-medium ml-1">11 kmph</span>
           </div>
-          
         </div>
       </div>
+      <div className="flex flex-row items-center justify-center space-x-4 text-white text-sm py-2">
+        <UilSun/>
+        <p className="font-light">
+          Rise:<span className="font-medium ml-1"> 06:45 AM</span>
+        </p>
+        <UilSun />
+        <p className="font-light">
+          Set:<span className="font-medium ml-1"> 07:35 PM</span>
+        </p>
+        <UilSun />
+        <p className="font-light">
+          High:<span className="font-medium ml-1"> 45°</span>
+        </p>
+        <UilSun />
+        <p className="font-light">
+          Low:<span className="font-medium ml-1"> 40°</span>
+        </p>
+      </div>
+
+      
     </div>
   );
 };
